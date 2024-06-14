34580a25d38eadd074fae8d77c35936cf4e00669
雨翀 孙 <milkandpotato@outlook.com>
2024-06-14 17:45:54 +0800

    排序

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,9 +12,12 @@
 
 // #include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 // #include <thread>
 #include "../include/template/Log.hpp"
 #include "../include/ThreadTest.hpp"
+#include "../include/NumberUtils.hpp"
 
 int main()
 {
@@ -50,34 +53,51 @@ int main()
   //  }
 
   // C++的多维数组
+  // {
+  //   // 二维数组
+  //   // 创建了5*5个数组，将会被分配在内存的25个区域
+  //   int **a2d = new int *[5];
+  //   for (int i = 0; i < 5; i++)
+  //   {
+  //     a2d[i] = new int[5];
+  //   }
+
+  //   // 二维数组（优化版）
+  //   // 创建了一个数组array指向一个长度为5*5的数组
+  //   // 这个在内存分配上是在同一个内存块上,避免了内存碎片
+  //   int *array = new int[5 * 5];
+  //   for (int i = 0; i < 5; i++)
+  //   {
+  //     for (int j = 0; j < 5; j++)
+  //     {
+  //       array[j + i * 5] = 2;
+  //     }
+  //   }
+
+  //   // 删除数组
+  //   // 不能直接使用delete[][]去删除二维数组
+  //   // delete[][] a2d;
+  //   for (int i = 0; i < 5; i++)
+  //   {
+  //     delete[] a2d[i];
+  //   }
+  //   delete[] a2d;
+  // }
+
+  // 排序
   {
-    // 二维数组
-    // 创建了5*5个数组，将会被分配在内存的25个区域
-    int **a2d = new int *[5];
-    for (int i = 0; i < 5; i++)
-    {
-      a2d[i] = new int[5];
-    }
+    std::string output = "";
 
-    // 二维数组（优化版）
-    // 创建了一个数组array指向一个长度为5*5的数组
-    // 这个在内存分配上是在同一个内存块上,避免了内存碎片
-    int *array = new int[5 * 5];
-    for (int i = 0; i < 5; i++)
-    {
-      for (int j = 0; j < 5; j++)
-      {
-        array[j + i * 5] = 2;
-      }
-    }
+    std::vector<int> array = {2, 5, 4, 7, 1};
+    std::sort(array.begin(), array.end()); // 一般排序
+    std::string decrease = NumberUtils::vector2String(std::ref(array));
+    std::cout << decrease << std::endl;
+    //使用自定义函数进行降序排列
+    // std::sort(array.begin(), array.end(), [](int a, int b) 
+    //           { return a > b; });
+    // std::string re
+    // std::cout << vector2String(std::ref(array)) << std::endl;
 
-    // 删除数组
-    // 不能直接使用delete[][]去删除二维数组
-    // delete[][] a2d;
-    for (int i = 0; i < 5; i++)
-    {
-      delete[] a2d[i];
-    }
-    delete[] a2d;
+    std::cin.get();
   }
 }
